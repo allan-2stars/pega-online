ab8100c07dcaf13bfc66bd1e6a87a33ca27f1a92
Allan Wang <wang.allan@outlook.com>
2019-03-18 22:04:20 +1100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+CoreUI based project.
---
+++ src/containers/DefaultLayout/DefaultFooter.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {
+  children: PropTypes.node
+};
+
+const defaultProps = {};
+
+class DefaultFooter extends Component {
+  render() {
+    // eslint-disable-next-line
+    const { children, ...attributes } = this.props;
+
+    return (
+      <React.Fragment>
+        <span>
+          <a href='https://pega.online'>Pega Online</a> &copy;{' '}
+          {new Date().getFullYear()}
+        </span>
+        <span className='ml-auto'>
+          Powered by <a href='https://pega.online'>Pega Online Technology</a>
+        </span>
+      </React.Fragment>
+    );
+  }
+}
+
+DefaultFooter.propTypes = propTypes;
+DefaultFooter.defaultProps = defaultProps;
+
+export default DefaultFooter;
